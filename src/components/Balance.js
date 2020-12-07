fa4cc71c16790f36aff2de0bc5e2ed331e397ac5
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2020-12-07 12:13:21 +0500

    update

---
+++ src/components/Balance.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <div>
+            <h1> Available Balance</h1>
+            <p>$499 </p>
+        </div>
+    )
+}
