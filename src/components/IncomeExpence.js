1be50c957a0304fd2363b88f17cb92df338ccb6b
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2020-12-09 17:32:28 +0500

    updated

--- src/components/IncomeExpence.js
+++ src/components/IncomeExpence.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const IncomeExpence = () => {
+  const {transactions} = useContext(GlobalContext)
+  const amouts =transactions.map(transaction =>transaction.amount)
+  const income = amouts.filter(item =>item > 0).reduce((acc, item)=>(acc += item), 0).toFixed(2)
+  const Expense = (amouts.filter(item =>item < 0).reduce((acc, item)=>(acc += item), 0)* -1).toFixed(2)
+  // console.log(income)
     return (
         <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
-          <p id="money-plus" className="money plus">+$0.00</p>
+          <p id="money-plus" className="money plus">{income}</p>
         </div>
         <div>
           <h4>Expense</h4>
-          <p id="money-minus" className="money minus">-$0.00</p>
+          <p id="money-minus" className="money minus">{Expense}</p>
         </div>
       </div>
 
